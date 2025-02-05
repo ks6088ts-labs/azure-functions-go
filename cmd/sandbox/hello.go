ff9822eed712a0508426fa87661e906e7a436649
ks6088ts <ks6088ts@gmail.com>
2025-02-05 13:37:27 +0900

    introduce gin

--- cmd/sandbox/hello.go
+++ cmd/sandbox/hello.go
@@ -22,10 +22,17 @@ THE SOFTWARE.
 package sandbox
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ks6088ts-labs/azure-functions-go/internal"
 
 	"github.com/spf13/cobra"
 )
@@ -36,35 +43,70 @@ var helloCmd = &cobra.Command{
 	Short: "SandBox Hello Command",
 	Long:  `This is a sandbox command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		listenAddr := ":8080"
+
+		// Get Port from environment
+		port := cmd.Flag("port").Value.String()
 		if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-			listenAddr = ":" + val
+			port = val
 		}
-		http.HandleFunc("/api/HttpExample", helloHandler)
-		log.Printf("About to listen on %s. Go to http://localhost%s/", listenAddr, listenAddr)
-		log.Fatal(http.ListenAndServe(listenAddr, nil))
+
+		srv := &http.Server{
+			Addr:    ":" + port,
+			Handler: getRouter(),
+		}
+
+		// Initializing the server in a goroutine so that
+		// it won't block the graceful shutdown handling below
+		go func() {
+			if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Printf("listen: %s\n", err)
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		// Creating a buffered channel to handle incoming signals
+		quit := make(chan os.Signal, 1)
+		// kill (no param) default send syscall.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Println("Shutting down server...")
+
+		// The context is used to inform the server it has 5 seconds to finish
+		// the request it is currently handling
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatal("Server forced to shutdown:", err)
+		}
+
+		log.Println("Server exiting")
 	},
 }
 
 func init() {
 	sandboxCmd.AddCommand(helloCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	helloCmd.Flags().IntP("port", "p", 8080, "Port number")
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
-	name := r.URL.Query().Get("name")
-	if name != "" {
-		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
-	}
-	fmt.Fprint(w, message)
+func getRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/api/version", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"version":  internal.Version,
+			"revision": internal.Revision,
+		})
+	})
+	router.GET("/api/http_example", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
+	return router
 }
